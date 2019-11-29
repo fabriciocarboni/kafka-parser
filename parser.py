043f371bdbc05955c94a63bf98b449fbaec55341
Patti Carboni <fabricio.patticarboni@bnc.ca>
2019-11-29 16:06:28 -0500

    Changes to find key in yaml file

--- parser.py
+++ parser.py
@@ -1,22 +1,13 @@
-
-# This script aims to read an yaml file wit some configs inside.
-# 1- Should find for the word 'kafkaPrincipal' as key and find ", "
-#     spaces right after any comma.
-# 2- If match, print warning message and the fixed value
-# 3- The script should find for 'quota' definition and do validations over
-#     the consumer_byte_rate and producer_byte_rate. We need to check if
-#     the value provided is correct. (waiting definition from Rajeev)
-# 4- After all validation, the script must run a command
-
-
 # -*- coding: utf-8 -*-
 
 import yaml
-import os
 import re
+import os
 
 
 def parse_kafka_principal(useQuotaBaseDir):
+
+    key = 'kafkaPrincipal'
 
     # check if directory is not empty
     if os.listdir(useQuotaBaseDir):
@@ -30,33 +21,37 @@ def parse_kafka_principal(useQuotaBaseDir):
                 except yaml.YAMLError as e:
                     print(e)
 
-    # looping through dict to find the list containing kafkaprincipal
-    for key, value in yml.items():
-        if isinstance(value, dict):
-            for k, v in value.items():
-                if isinstance(v, dict):
-                    for x, y in v.items():
-                        if isinstance(y, list):
-                            kafka_princ = y
+        def findkeys(node, kv):
+            if isinstance(node, list):
+                for i in node:
+                    for x in findkeys(i, kv):
+                        yield x
+            elif isinstance(node, dict):
+                if kv in node:
+                    yield node[kv]
+                for j in node.values():
+                    for x in findkeys(j, kv):
+                        yield x
 
-    for item in kafka_princ:
-        for key, value in item.items():
+    kafka_principal_items = list(findkeys(yml, key))
 
-            if(key == 'kafkaPrincipal'):
+    for item in kafka_principal_items:
 
-                match = re.search(r', ', value)
+        match = re.search(r', ', item)
 
-                if match:
-                    print(
-                        '[WARNING] There are spaces after comma in kafkaPrincipal parameter, please update it')
-                    clean = value.replace(", ", ",")
-                    print('[INFO] it should be like this => ' + clean)
+        if match:
+            print(
+                '[WARNING] There are spaces after comma in kafkaPrincipal parameter, please update it')
+            clean = item.replace(", ", ",")
+            print('[INFO] it should be like this => ' + clean)
 
     # execute command
     #cmd = 'kafka-configs.sh --zookeeper localhost:2181 --alter --add-config producer_byte_rate = $VALUE1, consumer_byte_rate = $vALUE2  --entity-type users --entity-name \'userPrincipal\''
 
 
 if __name__ == '__main__':
+
+    #print(list(findkeys(d, 'id')))
 
     #useQuotaBaseDir = '/home/fabricio/Documents/kafka-parser/'
     useQuotaBaseDir = 'C:/Users/patf001/Documents/my_stuff/kafka-parser/'
